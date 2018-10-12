6d7850fddb78627e7c95d70d6bf47339e4b35961
Vansika Pareek <vansikapareek@yahoo.in>
2018-10-12 14:48:27 +0530

    configure passwords

---
+++ config.py
@@ -0,0 +1,9 @@
+DATABASE_HOST = 'localhost'
+DATABASE_USER = 'root'
+DATABASE_PASSWORD = 'Your password'
+DATABASE_NAME = 'series'
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_SENDER = 'Your email'
+EMAIL_PASSWORD = 'Your password'
